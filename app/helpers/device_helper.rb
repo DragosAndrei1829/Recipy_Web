90c61dba0b0348f4987ea5364a592d6d86f9215d
Dragos Andrei <dragosandrei@Dragoss-MacBook-Air-2.local>
2025-12-04 13:01:55 +0200

    Add dedicated mobile layout with simplified cards

---
+++ app/helpers/device_helper.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module DeviceHelper
+  def mobile_device?
+    request.user_agent =~ /Mobile|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i
+  end
+  
+  def tablet_device?
+    request.user_agent =~ /iPad|Android(?!.*Mobile)/i
+  end
+  
+  def desktop_device?
+    !mobile_device? && !tablet_device?
+  end
+end
+
